c1db707e0e27f35453a62211bc99684f4e02314f
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-05-17 14:37:00 -0300

    refactor: remove material-ui

--- src/App.js
+++ src/App.js
@@ -1,93 +1,82 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
 import { createBrowserHistory } from 'history';
-import theme from './theme';
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  MuiThemeProvider,
-  Container,
-  Avatar,
-  IconButton,
-} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import NoteAddIcon from '@material-ui/icons/NoteAdd';
+
+import Styles from './styles';
 
 import SurveyDetails from './pages/survey/Details';
 import SurveyList from './pages/survey/List';
 import SurveyForm from './pages/survey/Form';
 
 import DefaultAvatar from './assets/images/default-avatar.png';
+import MoreVertIcon from './assets/images/icon-more-vert.svg';
+import NoteAddIcon from './assets/images/icon-note-add.svg';
+
+const Header = styled.header`
+  width: 100%;
+  background: #00434d;
+  margin-bottom: 30px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+const AppBar = styled.nav`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 {
+    font-size: 48px;
+    font-weight: bold;
+    color: #fff;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    a {
+      padding: 10px;
+      margin-left: 15px;
+    }
+  }
+`;
 
 const history = createBrowserHistory(); // TO DO: move to services folder
 
-const useStyles = makeStyles({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    width: '100%',
-  },
-  toolbar: {
-    display: 'flex',
-    justifyContent: 'space-around',
-  },
-  titleSpacing: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-  title: {
-    fontWeight: 'bold',
-    color: '#fff',
-  },
-  icons: {
-    display: 'flex',
-  },
-});
-
 const App = () => {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <MuiThemeProvider theme={theme}>
-        <Router history={history}>
-          <AppBar className={classes.appBar} position="fixed">
-            <Toolbar className={classes.toolbar}>
-              <Typography variant="h4" className={classes.title}>
-                <IconButton href="/" color="inherit">
-                  GoSurvey
-                </IconButton>
-              </Typography>
-              <div className={classes.icons}>
-                <IconButton href="/add" color="inherit">
-                  <NoteAddIcon fontSize="large" />
-                </IconButton>
-                <IconButton color="inherit">
-                  <MoreVertIcon fontSize="large" />
-                </IconButton>
-                <IconButton color="inherit">
-                  <Avatar alt="Login" src={DefaultAvatar} />
-                </IconButton>
-              </div>
-            </Toolbar>
+    <>
+      <Router history={history}>
+        <Styles />
+        <Header>
+          <AppBar className="container">
+            <h1>GoSurvey</h1>
+            <div>
+              <a href="/">
+                <img src={NoteAddIcon} alt="" />
+              </a>
+              <a href="/">
+                <img src={MoreVertIcon} alt="" />
+              </a>
+              <a href="/">
+                <img src={DefaultAvatar} alt="" />
+              </a>
+            </div>
           </AppBar>
-          <main className={classes.content}>
-            <div className={classes.titleSpacing} />
-            <Container maxWidth="lg">
-              <Switch>
-                <Route path="/" exact component={SurveyList} />
-                <Route path="/details" component={SurveyDetails} />
-                <Route path="/add" component={SurveyForm} />
-              </Switch>
-            </Container>
-          </main>
-        </Router>
-      </MuiThemeProvider>
-    </div>
+        </Header>
+        <main>
+          <Switch>
+            <Route path="/" exact component={SurveyList} />
+            <Route path="/details" component={SurveyDetails} />
+            <Route path="/add" component={SurveyForm} />
+          </Switch>
+        </main>
+      </Router>
+    </>
   );
 };
 
